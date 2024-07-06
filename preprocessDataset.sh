9070a1dfaaf115214c71888de83a8c522ddde765
Matthew Delaney <MattDelaney_948@protonmail.com>
2024-07-06 18:11:59 -0400

    Proprocessing script written

---
+++ preprocessDataset.sh
@@ -0,0 +1,40 @@
+DATASET=./irisDataset/iris.data
+OUT_DATASET=./irisDataset/preprocessedStdSetosa.data
+
+SEPAL_LEN_MEAN=5.84
+SEPAL_LEN_STD=0.83
+SEPAL_WID_MEAN=3.05
+SEPAL_WID_STD=0.43
+PETAL_LEN_MEAN=3.76
+PETAL_LEN_STD=1.76
+PETAL_WID_MEAN=1.20
+PETAL_WID_STD=0.76
+
+function main() {
+    cat $DATASET | shuffle | removeCommas | standardize | oneHotSetosa > $OUT_DATASET
+}
+
+function removeCommas() {
+    sed 's/,/ /g'
+}
+
+function shuffle() {
+    sort -R
+}
+
+function standardize() {
+    awk "{
+        stdSepalLen = (\$1 - $SEPAL_LEN_MEAN) / $SEPAL_LEN_STD
+        stdSepalWid = (\$2 - $SEPAL_WID_MEAN) / $SEPAL_WID_STD
+        stdPetalLen = (\$3 - $PETAL_LEN_MEAN) / $PETAL_LEN_STD
+        stdPetalWid = (\$4 - $PETAL_WID_MEAN) / $PETAL_WID_STD
+        class = \$5
+        print stdSepalLen, stdSepalWid, stdPetalLen, stdPetalWid, class
+    }"
+}
+
+function oneHotSetosa() {
+    sed 's/Iris-setosa/1/' | sed 's/Iris.*/0/'
+}
+
+main
